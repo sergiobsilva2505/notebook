4c93c9256f2cfeb267312f1cd03a72365454e1af
Sergio Bezerra da Silva <sergio.bezerra@outlook.com>
2022-01-19 15:42:56 -0300

    first commit

---
+++ src/main/java/br/com/sbs/notebook/NotebookApplication.java
@@ -0,0 +1,13 @@
+package br.com.sbs.notebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotebookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sbs/notebook/NotebookApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sbs.notebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotebookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
